f5cef655715560de5bde81f721e9f6043c8f1e0c
Pierre LIGONNIERE <pierre.ligonniere@edu.devinci.fr>
2021-02-04 16:48:37 +0100

    encore un peu

---
+++ GestionSalarie.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pizza_du_domaine
+{
+    /// <summary>
+    /// Logique d'interaction pour GestionSalarie.xaml
+    /// </summary>
+    public partial class GestionSalarie : Page
+    {
+        public GestionSalarie()
+        {
+            InitializeComponent();
+        }
+    }
+}
